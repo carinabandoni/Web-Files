40eddefc10f7b90157d9c8a4e482a3e1f70a8538
carinabandoni <caba7055@colorado.edu>
2021-11-16 13:53:53 -0700

    fixeddd

--- Week 11 Web/index.js
+++ Week 11 Web/index.js
@@ -5,5 +5,17 @@ let pic;
 
 for(i=0;i<pics;i++){
   pic = 1+i;
-document.getElementById('portfolio').innerHTML += "<img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' />";
+document.getElementById('portfolio').innerHTML += "<divclass= 'pimage'> <img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' /> </div>";
+}
+
+let displaydetails = false;
+document.getElementsById('portfolio').addEventListener("click",showDetails);
+
+function showDeatails(){
+  document.getElementById('details').style.display ="block";
+  document.getElementById('detailsoverlay').style.display ="block";
+  document.getElementById('detailsoverlay').addEventListener('click',clickedOverlay)
+
+  // this is where we eneded in class!
+
 }
